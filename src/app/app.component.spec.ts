20748cb6c2a99c491d4fefa768af50eb313eafdb
royalkuriyakosem <royalkuriyakosem@gmail.com>
2025-01-06 14:49:22 +0530

    Add new files and update existing files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ecom' title`, () => {
+  it(`should have the 'ecommerce' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ecom');
+    expect(app.title).toEqual('ecommerce');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ecom');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ecommerce');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from "./ui/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ecom';
+  title = 'ecommerce';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
+
 }
